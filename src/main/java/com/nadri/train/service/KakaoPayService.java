ea3b91cb7f34e5d12030650193cfe2d67fbe338c
songhees <osh4880@gmail.com>
2022-02-18 23:32:01 +0900

    [UPDATE] kakaoPay 수정중

---
+++ src/main/java/com/nadri/train/service/KakaoPayService.java
@@ -0,0 +1,15 @@
+package com.nadri.train.service;
+
+import org.springframework.stereotype.Service;
+
+import com.nadri.train.kakaoPay.ReadyResponse;
+
+
+@Service("train")
+public class KakaoPayService {
+	
+	public ReadyResponse readyPayment() {
+		
+		return null;
+	}
+}
